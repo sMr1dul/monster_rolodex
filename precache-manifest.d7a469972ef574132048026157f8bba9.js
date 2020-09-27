1f541822fc3d51022b356ce411585f979b849c3a
sMr1dul <smridul198@gmail.com>
2020-09-27 17:09:41 +0530

    Updates

---
+++ precache-manifest.d7a469972ef574132048026157f8bba9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b73c6eaa9c36dccb9974296c9c4856a4",
+    "url": "/monster_rolodex/index.html"
+  },
+  {
+    "revision": "0fc96b74823bd0fc520e",
+    "url": "/monster_rolodex/static/css/main.8482f028.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monster_rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster_rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fc96b74823bd0fc520e",
+    "url": "/monster_rolodex/static/js/main.27c16444.chunk.js"
+  },
+  {
+    "revision": "97ca52abc78853e16b71",
+    "url": "/monster_rolodex/static/js/runtime-main.23622e35.js"
+  }
+]);
